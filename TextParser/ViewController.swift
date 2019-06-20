4c0b7d293d24914dc96177416c1fb38d6bca3cba
Jean-Luc David <jldavid.mail@gmail.com>
2019-06-20 04:15:14 -0400

    Updated parser to use filter/contains

--- TextParser/ViewController.swift
+++ TextParser/ViewController.swift
@@ -2,32 +2,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //  NOTES:
+    //  The current approach is very simple, but x number of search phrases leads to O(x * n).
+    //  A recursive PrefixTree/Tries might be faster with a larger set of words in a dictionary.
+    
     @IBOutlet var searchField: UITextField!
     @IBOutlet var resultsTextView: UITextView!
     
-    let tree = PrefixTree()
+    let dictionary = ["apple", "pie", "shoe", "second", "secondary"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupGestures()
         setupTextField()
         loadViews()
-        loadWords()
     }
 
     @IBAction func parse(_ sender: Any) {
-        print(tree.contains(word: "best"))
-        print(tree.contains(word: "bes"))
+        if let str = searchField.text {
+            let results = dictionary.filter{str.contains($0)}
+            if results.isEmpty {
+                resultsTextView.text = "No results found."
+            } else {
+                resultsTextView.text = results.joined(separator: "\n")
+            }
+        } else {
+           resultsTextView.text = "Error - can't parse the text."
+        }
     }
     
     func loadViews() {
         searchField.returnKeyType = .done
         resultsTextView.isSelectable = false
-    }
-    
-    func loadWords() {
-        tree.insert(word: "best")
-        tree.insert(word: "bes")
     }
     
     func dismissKeyboard() {
